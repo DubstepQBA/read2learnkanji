09a22b4515ffaf511ec809bc7194417f8ab59156
RomainMarceDesj <desjonqueres@keio.jp>
2025-09-23 10:36:32 +0900

    Changed port

--- src/furigana-api/app.py
+++ src/furigana-api/app.py
@@ -297,6 +297,6 @@ def analyze_text():
     return jsonify({"data": [], "totalLength": 0})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(host="0.0.0.0", debug=True)
 else:
     print("hello")
